da83e1e7a999950a5080c1124c6499efb20e866c
Felixmensah <felixmensah6891@gmail.com>
2023-03-14 08:45:18 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,11 +9,9 @@
  */
 
 char *str_concat(char *s1, char *s2)
-
 {
 
 	char *conct;
-
 	int i, ci;
 
 	if (s1 == NULL)
@@ -34,32 +32,22 @@ char *str_concat(char *s1, char *s2)
 
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
-
 		if (conct == NULL)
-
 		return (NULL);
 
 	i = ci = 0;
 
 	while (s1[i] != '\0')
-
 	{
-
 		conct[i] = s1[i];
-
 		i++;
-
 	}
 	while (s2[ci] != '\0')
 	{
-
 	conct[i] = s2[ci];
-
 	i++, ci++;
-
 	}
 	conct[i] = '\0';
-
 	return (conct);
 
 }
